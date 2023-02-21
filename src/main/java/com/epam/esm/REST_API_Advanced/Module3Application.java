f3e0aad0aa553c8b8232373930ea967d59c5e0e8
Vita1ius <vitalikbalushak@gmail.com>
2023-02-21 18:54:13 +0200

    add starter tomcat

--- src/main/java/com/epam/esm/REST_API_Advanced/Module3Application.java
+++ src/main/java/com/epam/esm/REST_API_Advanced/Module3Application.java
@@ -2,9 +2,10 @@ package com.epam.esm.REST_API_Advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Module3Application {
+public class Module3Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Module3Application.class, args);
