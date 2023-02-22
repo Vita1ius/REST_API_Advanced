85388767f1f6ea1048948650d4c67f8ec9452bc9
Vita1ius <vitalikbalushak@gmail.com>
2023-02-22 12:50:53 +0200

    fix

--- src/main/java/com/epam/esm/REST_API_Advanced/Module3Application.java
+++ src/main/java/com/epam/esm/REST_API_Advanced/Module3Application.java
@@ -2,6 +2,7 @@ package com.epam.esm.REST_API_Advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,5 +11,8 @@ public class Module3Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Module3Application.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Module3Application.class);
+    }
 }
